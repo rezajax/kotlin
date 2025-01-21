00c4c0cbb4b6be2cbffe2cc0ad371c6f9d646ac8
Reza Jax <rezajaxg@gmail.com>
2025-01-21 18:37:21 +0400

    second

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "ir.rezajax"
@@ -10,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,31 @@
 package ir.rezajax
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
 fun main() {
-    println("Hello World!")
+
+}
+
+
+fun fetchMovies (stringUrl: String) : String {
+    val client = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder(URI(urlCreator(550))).build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
+}
+
+
+
+fun urlCreator (movieId: Int) : String {
+    val api = buildString {
+        append("https://api.themoviedb.org/3/movie/")
+        append(movieId)
+        append("?api_key=")
+        append("b955b1053d8c0d7dca019181fa21c28e")
+    }
+    return api
 }
